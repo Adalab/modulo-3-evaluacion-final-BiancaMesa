9c1bc13397c94bfea702db0d86dfc59bd7a64122
BiancaMesa <bianca.koraa@gmail.com>
2024-03-30 13:36:24 +0100

    no matches found, changed image and put the text on top of the image with position relative

--- src/components/NoNameFound.jsx
+++ src/components/NoNameFound.jsx
@@ -1,17 +1,19 @@
 import "../scss/components/NoNameFound.scss";
 import notFoundGif from "../images/gif2.gif";
+import rickBadLook from "../images/rick_bad_look.png";
 
 function NoNameFound({message}) {
   return (
     <div className="noNameFoundContainer">
-      {message && <p className="noNameFoundContainer__message">{message}</p>}
+      {message && <p className="noNameFoundContainer__message1">{message}</p>}
       {message && 
         <p className="noNameFoundContainer__message2">
           Give it another try!
           {/* <i className="wink fa-solid fa-face-smile-wink"></i> */}
         </p>
       }
-      {message && <img className="noNameFoundContainer__gif" src={notFoundGif} alt="gif-IThoughtItWasCool" /> }
+      {/* {message && <img className="noNameFoundContainer__gif" src={notFoundGif} alt="gif-IThoughtItWasCool" /> } */}
+      {message && <img className="noNameFoundContainer__gif" src={rickBadLook} alt="rickBadLook" /> }
     </div>
   );
 }

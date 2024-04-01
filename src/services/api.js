b2ac41865aab9538a8d2a256e6e9bef9ace8c012
BiancaMesa <bianca.koraa@gmail.com>
2024-04-01 13:49:47 +0200

    comments deleted from api.js y localStorage.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
-//componente para hacer la petición al servidor. El nombre de la carpeta no empieza por mayúscula porque no estamos creando un componente sino una petición normal
+
 
 //función asíncrona que hace la petición al servidor 
 const callToApi = () => {
     return fetch ("https://rickandmortyapi.com/api/character")
-    //return fetch ("https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json")
         .then ((response) => response.json())
         .then ((data) => {
             //nos creamos un array de objetos que contenga sólo la información que necesitamos de la API 
-            //map va a recorrer cada elemento del array de la API, que en este caso cada elemento es un objeto, y nos va a devolver un objeto nuevo con la información de la API que nosotras necesitamos
             const parsedCharacters = data.results.map(character => {
                 return {
                     image: character.image,
@@ -24,5 +22,4 @@ const callToApi = () => {
         });
 };
 
-//como esta función la queremos usar fuera, la vamos a exportar 
 export default callToApi; 
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,6 +1,4 @@
 // Función que obtiene una propiedad del local storage
-// Si esta propiedad no existe porque es la primera vez que la usuaria entra en la página, la función devuelve el valor de defaultValue
-// Que esta función devuelva un valor por defecto es una cómoda manera de trabajar, así esta comprobación no la tenemos que hacer en App.js
 const get = (key, defaultValue) => {
     const localStorageData = localStorage.getItem(key);
     if (localStorageData === null) {
@@ -26,11 +24,7 @@ const get = (key, defaultValue) => {
     localStorage.clear();
   };
   
-  // Creamos un objeto temporal, que es el que queremos exportar
-  // Este objeto tiene una propiedad get cuyo valor es la función get
-  // Este objeto tiene una propiedad set cuyo valor es la función set
-  // Este objeto tiene una propiedad remove cuyo valor es la función remove
-  // Este objeto tiene una propiedad clear cuyo valor es la función clear
+  // Objeto que tiene como valores las funciones que hemos creado arriba 
   const objectToExport = {
     get: get,
     set: set,
